502f7945c0a993d3a34efaf7c2a29556ad2ac420
wb_liuyinfei <wb_liuyinfei@meituan.com>
2022-10-09 17:13:53 +0800

    测试部署

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'blog',
+    description: '个人网站',
+    base: '/blog/',
+    theme: 'reco',
+    locales: {
+        '/': {
+          lang: 'zh-CN'
+        }
+    },
+  themeConfig: {
+    }
+}
